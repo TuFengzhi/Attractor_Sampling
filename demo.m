851beae8454d682567d42992a0e3ff5abf8ec6f6
zubeneschamali <tufengzhi@gmail.com>
2024-05-19 19:33:55 +0800

    add statNetResponse

--- demo.m
+++ demo.m
@@ -206,6 +206,72 @@ function InputSet = makeNetInput(InputSet, NetPars, outArgs)
     
 end
 
+function NetStat = statNetResponse(BumpPos, OHeight, UHeight, O, NetPars, NetStat)
+    % Get the statistics of network responses
+    
+    % The input NetStat is a empty struct, the name of fields is used to
+    % indicate which variables to output.
+    
+    % Author: Wen-Hao Zhang, June-7-2017
+    % wenhaoz1@andrew.cmu.edu
+    % @Carnegie Mellon University
+    
+    if isfield(NetStat, 'sumSqU')
+        sumSqU = U;
+        sumSqU(sumSqU<0) = 0;
+        sumSqU = squeeze(sum(sumSqU.^pwr,1));
+        NetStat.sumSqU = sumSqU;
+    end
+    
+    if isfield(NetStat, 'OAvgXTime')
+        NetStat.OAvgXTime = mean(O,3);
+    end
+    
+    if isfield(NetStat, 'OStdXTime')
+        NetStat.OStdXTime = std(O,[],3);
+    end
+    
+    if isfield(NetStat, 'BumpPos')
+        NetStat.BumpPos = BumpPos;
+    end
+    
+    if isfield(NetStat, 'OHeight')
+        NetStat.OHeight = OHeight;
+    end
+    
+    if isfield(NetStat, 'OHeightAvg')
+        OHeight = OHeight(:,NetPars.tStat/NetPars.dt+1:end, :);
+        NetStat.OHeightAvg = mean(reshape(OHeight, size(OHeight, 1), []), 2);
+        NetStat.OHeightAvg = squeeze(NetStat.OHeightAvg);
+    end
+    
+    if isfield(NetStat, 'UHeightAvg')
+        UHeight = UHeight(:,NetPars.tStat/NetPars.dt+1:end, :);
+        NetStat.UHeightAvg = mean(reshape(UHeight, size(OHeight, 1), []), 2);
+        NetStat.UHeightAvg = squeeze(NetStat.UHeightAvg);
+    end
+    
+    if isfield(NetStat, 'meanBumpPos')
+        BumpPos = BumpPos(:,NetPars.tStat/NetPars.dt+1:end, :);
+        BumpPos = reshape(BumpPos, size(BumpPos,1), []);
+        [~, meanBumpPos, varBumpPos, concBumpPos, mrlBumpPos] = statBumpPos(BumpPos, NetPars, 'BumpPos');
+        
+        NetStat.meanBumpPos = meanBumpPos;
+    end
+    
+    if isfield(NetStat, 'varBumpPos')
+        NetStat.varBumpPos  = varBumpPos;
+    end
+    
+    if isfield(NetStat, 'concBumpPos')
+        NetStat.concBumpPos = concBumpPos;
+    end
+    
+    if isfield(NetStat, 'mrlBumpPos')
+        NetStat.mrlBumpPos  = mrlBumpPos;
+    end
+end
+
 function [InputSet, NetStat] = simCoupledAttractors1(InputSet, NetPars, outArgs)
     % A decentralized system for information integration
     % The whole system is composed of several networks, with each is modeled as
@@ -340,6 +406,8 @@ function [InputSet, NetStat] = simCoupledAttractors1(InputSet, NetPars, outArgs)
         InputSet.O = OArray;
     end
 end
+
+
 
 function [BumpPos, meanBumpPos, varBumpPos, concBumpPos, mrlBumpPos] = statBumpPos(Input, NetPars, inputName)
     % Calculate the bump position by projecting network
